39f88805715e2bb01567396ffff6adef1947edb7
Jeff <horusofoz@gmail.com>
2018-10-04 22:06:23 +0930

    * Created Player script * Added player Move method

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float moveSpeed = 5f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        Move();
+	}
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal");
+        var deltaY = Input.GetAxis("Vertical");
+        var newXPos = transform.position.x + deltaX * Time.deltaTime * moveSpeed;
+        var newYPos = transform.position.y + deltaY * Time.deltaTime * moveSpeed;
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
