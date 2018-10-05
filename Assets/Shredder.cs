a58e5529a109c6b1d8000ea88a5f1e96bc6b8ad1
Jeff <horusofoz@gmail.com>
2018-10-05 23:58:08 +0930

    * Created GameObject Shredder above play area * Shredder destroys laser objects via OnTriggerEnter2D > Destroy Collision.GameObject

---
+++ Assets/Shredder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collided with " + collision.gameObject.name);
+        Destroy(collision.gameObject);
+    }
+}
