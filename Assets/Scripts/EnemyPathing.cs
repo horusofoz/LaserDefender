ab76d220c832ba86b7301bb025057b988595323e
Jeff <horusofoz@gmail.com>
2018-10-06 23:21:44 +0930

    * Modified WaveConfig to input path and have method that returns list of waypoints contained in path * Created a second path to test functionality

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class EnemyPathing : MonoBehaviour {
 
     // Configuration Parameters
-    [SerializeField] List<Transform> waypoints;
+    [SerializeField] WaveConfig waveConfig;
+    List<Transform> waypoints;
     [SerializeField] float moveSpeed = 2f;
     int waypointIndex = 0;
 
 	// Use this for initialization
 	void Start () 
 	{
+        waypoints = waveConfig.GetWaypoints();
         transform.position = waypoints[waypointIndex].transform.position;
 	}
 	
