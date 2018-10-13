e3b58ed787db220eff6e97e3ff992e12f180d010
Jeff <horusofoz@gmail.com>
2018-10-14 01:45:40 +1030

    * Added shield pick up prefab * Added animation for when picked up * Added PowerUp.cs to trigger animation, add shieldHealth and destroy shield gameobject * Added animation for shield power up

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour {
+
+    private Rigidbody2D rigidbody2D;
+    private bool active = true;
+    [SerializeField] private int dropSpeed = 3;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") && active == true)
+        {
+            PickUp();
+        }
+    }
+
+    private void PickUp()
+    {
+        // Prevent pickup being double dipped on
+        active = false;
+        
+        // Spawn a cool effect
+        Animator animator = GetComponent<Animator>();
+        animator.Play("Shield Picked Up");
+
+        // Apply effect to player
+        GameSession gameSession = FindObjectOfType<GameSession>();
+        gameSession.AddShieldHealth(gameSession.shieldLayer01);
+        gameSession.AddToScore(1000);
+        
+
+        // Remove power up object
+        Destroy(gameObject, 1f);
+    }
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.velocity = new Vector2(transform.position.x, -dropSpeed);
+    }
+}
